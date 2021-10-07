79b3f72a6ef3efd68945e2854b5997b0d8e2eb8a
RodnyMorales <rodnymorales739@gmail.com>
2021-10-06 18:37:21 -0600

    hola

--- apps/storage/views.py
+++ apps/storage/views.py
@@ -3,5 +3,5 @@ from .models import *
 # Create your views here.
 
 def index(request):
-    allproduct = Equipo.objects.all()
-    return render(request, 'index.html', {'allproduct':allproduct})
+    allequipo = Equipo.objects.all()
+    return render(request, 'index.html', {'allequipo':allequipo})
